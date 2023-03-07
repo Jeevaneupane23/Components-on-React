805b48f05e34fbdb415d7db5ede9335b3758b2c8
Jeevan Neupane <078bct036@student.ioepc.edu.np>
2023-03-07 14:05:23 +0545

    icons to be made

--- src/Components/Accordion.js
+++ src/Components/Accordion.js
@@ -3,14 +3,17 @@ import { useState } from "react";
 
 function Accordion({ items }) {
     const [expandedIndex,setExpandedIndex]=useState(0);
-
+    const handleClick=(nextindex)=>{
+        setExpandedIndex(nextindex);
+       }
     const renderedItems = items.map((item,index) => {
        const isExpanded=index===expandedIndex;
-       
+       console.log(isExpanded)
+     
    
         return (
             <div key={item.id}>
-                <div>{item.label}</div>
+                <div onClick={()=>handleClick(index)}>{item.label}</div>
                 {isExpanded &&  <div>{item.content}</div>};
               
             </div>
