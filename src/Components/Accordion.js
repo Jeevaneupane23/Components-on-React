c29fa6feef51d83f5fa15dbd5339f6fe1df52c1e
Jeevan Neupane <078bct036@student.ioepc.edu.np>
2023-03-09 05:09:03 +0545

    accordion was made

--- src/Components/Accordion.js
+++ src/Components/Accordion.js
@@ -1,20 +1,27 @@
 import { useState } from "react";
+import { BsFillCaretDownFill } from "react-icons/bs";
+import { BsFillCaretLeftFill } from "react-icons/bs";
 
 
 function Accordion({ items }) {
-    const [expandedIndex,setExpandedIndex]=useState(0);
+    const [expandedIndex,setExpandedIndex]=useState(-1 );
     const handleClick=(nextindex)=>{
         setExpandedIndex(nextindex);
+        if(nextindex===expandedIndex){
+            setExpandedIndex()
+        }
        }
     const renderedItems = items.map((item,index) => {
        const isExpanded=index===expandedIndex;
-       console.log(isExpanded)
+     const icon=<span className="text-4xl" >
+        {isExpanded?<BsFillCaretDownFill/>:<BsFillCaretLeftFill/>}
+     </span>
      
    
         return (
-            <div key={item.id}>
-                <div onClick={()=>handleClick(index)}>{item.label}</div>
-                {isExpanded &&  <div>{item.content}</div>};
+            <div  key={item.id}>
+                <div className="flex p-3 bg-grey-50 border-b items-center cursor-pointer justify-between" onClick={()=>handleClick(index)}>{item.label}{icon}</div>
+                {isExpanded &&  <div className="border-b p-5 ">{item.content}</div>}
               
             </div>
         )
@@ -23,7 +30,7 @@ function Accordion({ items }) {
 
 
     return (
-        <div>
+        <div className="border-x border-t rounded">
             {renderedItems}
 
         </div>
