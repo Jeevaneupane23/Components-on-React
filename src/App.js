8c36794cc0f18e9f7daba3ed49b2fba995cb5e52
Jeevan Neupane <078bct036@student.ioepc.edu.np>
2023-03-09 08:33:44 +0545

    Move to the parents while makeing the dropdown

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ function App(){
 
   const options=[
     {
+      id:1,
       label:"Red",
       value:"red"
     },
     {
+      id:2,
       label:"Green",
       value:"green"
     },
     {
+      id:3,
       label:"Blue",
       value:"blue"
     }
--- src/Components/DropDown.js
+++ src/Components/DropDown.js
@@ -1,15 +1,46 @@
+import { useState } from "react";
 
 
 
 
-function DropDown({options}){
+function DropDown({ options }) {
+    const [isOpen, setIsOpen] = useState(false)
+ 
+    const renderedOptions = options.map((option) => {
+        return (<div onClick={()=>{handleOptionClick(option)}} key={option.id} id={option.value}>
+            {option.label}
+        </div>)
+    })
+    function handleOptionClick(option) {
+        setIsOpen((currentValue)=>{
+            return !currentValue;
+        })
+
+  
+        
+    }
+  
+    function handleClick() {
+        setIsOpen((currentValue)=>{
+            return !currentValue;
+        })
+    }
+  
+
+
+ 
 
 
 
 
 
     return (
-  <div></div>
+        <div>
+            <div onClick={handleClick}>Select...</div>
+            <div>
+                {isOpen && renderedOptions}
+            </div>
+        </div>
     )
 }
 export default DropDown;
